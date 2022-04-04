4efa3991efcbe350c5ff074df74d1515ff535eea
Matthew Stewart <stewartmatthew6@gmail.com>
2022-04-03 20:50:30 -0400

    Cracker Cavern 3 Solved

--- 2022/script-decompiler/solver.py
+++ 2022/script-decompiler/solver.py
@@ -1,38 +1,47 @@
 #!/usr/bin/python3
+import binascii
 
 MULTIPLIERS_1 = [0x0049, 0x0061, 0x000d, 0x0029, 0x0043, 0x0065, 0x0059, 0x008b, 0x0047, 0x0053]
 ADDERS_1 =      [0x1b39, 0x18df, 0x13eb, 0x11ef, 0x1145, 0x12df, 0x0dfd, 0x13af, 0x149f, 0x0fef, 0x0fb5]
-TARGET_VALUE_1 = 0xb0ef
 
 MULTIPLIERS_2 = [0x003b, 0x00b5, 0x007f, 0x00a3, 0x0067, 0x00a3, 0x0095, 0x00c1, 0x00d3, 0x0097]
 ADDERS_2      = [0x0539, 0x0e75, 0x11fb, 0x1237, 0x125f, 0x107b, 0x1951, 0x1b47, 0x151f, 0x14b1, 0x13eb]
+
+# True values
+TARGET_VALUE_1 = 0xb0ef
 TARGET_VALUE_2 = 0xd4b9
 
-# Find values of "a, b, c, ..." in equation "ax1 + bx2 + cx3 + ... + k = 0";
-# where "xn" = password[n] + ADDERS[n]
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def factorizeConstants(constants):
+    for i in range(10):
+        print(prime_factors(constants[i]))
+
+# Find values of "a, b, c, ..." in equation "ap1 + bp2 + cp3 + ... + k = 0";
+# where "pn" = password[n]
 def findConstants(ADDERS, MULTIPLIERS, TARGET_VALUE):
+    lastConstant = (ADDERS[-1] - TARGET_VALUE) & 0xffff
     retvals = []
-    retvals.append((ADDERS[-1] - TARGET_VALUE) & 0xffff)
     mult = 1
     for index in range(9, -1, -1):
         mult *= MULTIPLIERS[index]
         mult &= 0xffff
         retvals.append(mult)
-    return list(reversed(retvals))
-
-# Given two sets of constants of the form "ax1 + bx2 + cx3 + ... + k = 0",
-# Returns a new set of constants for "ap1 + bp2 + bp3 + ... + k = 0", where "pn"
-# is a character in the password.
-def combineConstants(constants1, constants2):
-    lastConstant = constants1[-1] + constants2[-1]
-    retvals = []
-
-    for i in range(10):
-        v = (constants1[i] + constants2[i]) & 0xffff
-        retvals.append(v)
-        lastConstant += constants1[i] * ADDERS_1[i] + constants2[i] * ADDERS_2[i]
+        lastConstant += mult * ADDERS[index]
         lastConstant &= 0xffff
-
+    retvals = list(reversed(retvals))
     retvals.append(lastConstant)
     return retvals
 
@@ -58,6 +67,7 @@ def getPasswordHash(password, ADDERS, MULTIPLIERS):
         total &= 0xffff
 
     total += ADDERS[-1]
+    total &= 0xffff
     return total
 
 def sumConstants(password, constants):
@@ -67,8 +77,8 @@ def sumConstants(password, constants):
         total &= 0xffff
     return total
 
-def solve(constants):
-    ASSUMED = 8
+def solveSingleEquation(constants):
+    ASSUMED = 1
     password_base = bytearray([0xbb] * ASSUMED)
     total = constants[-1]
     retvals = []
@@ -82,9 +92,55 @@ def solve(constants):
             tryvals.append(n)
 
         if sumConstants(tryvals, constants) == 0:
-            return tryvals
+            yield tryvals
 
-    return -1
+
+tried = 0
+password = []
+
+def solveBothEquations(start=0):
+    global tried
+
+    #print('FRESH START')
+
+    #assert(getPasswordHash(password, ADDERS_1, MULTIPLIERS_1) == TARGET_VALUE_1)
+    for n1 in range(start, 10):
+        p1L = password[n1]
+        for p1N in range(0, 256):
+            if p1N == p1L:
+                continue
+
+            for n2 in range(n1 + 1, 10):
+                p2L = password[n2]
+                c1 = constants1[n1]
+                c2 = constants1[n2]
+
+                diff1 = c1 * (p1N - p1L)
+                if (diff1 % c2) != 0:
+                    continue
+                p2N = (-diff1 // c2 + p2L) & 0xffff
+                if p2N < 0 or p2N >= 256:
+                    continue
+                diff2 = c2 * (p2N - p2L)
+                #print('DIFF1: ' + hex(diff1))
+                #print('DIFF2: ' + hex(diff2))
+                password[n1] = p1N
+                password[n2] = p2N
+
+                #assert(getPasswordHash(password, ADDERS_1, MULTIPLIERS_1) == TARGET_VALUE_1)
+
+                if sumConstants(password, constants2) == 0:
+                    print('SOLUTION: ' + binascii.hexlify(password).decode())
+
+                tried += 1
+                if tried % 50 == 0:
+                    print('Tried ' + str(tried) + ' values')
+
+                solveBothEquations(n1 + 1)
+                password[n1] = p1L
+                password[n2] = p2L
+                break
+
 
 assert(getPasswordHash('ABC', ADDERS_1, MULTIPLIERS_1) == 0xa7da)
 assert(getPasswordHash('ABC', ADDERS_2, MULTIPLIERS_2) == 0xc4fe)
@@ -92,9 +148,28 @@ assert(getPasswordHash('ABC', ADDERS_2, MULTIPLIERS_2) == 0xc4fe)
 constants1 = findConstants(ADDERS_1, MULTIPLIERS_1, TARGET_VALUE_1)
 constants2 = findConstants(ADDERS_2, MULTIPLIERS_2, TARGET_VALUE_2)
 
-constants = combineConstants(constants1, constants2)
+assert(getPasswordHash(next(solveSingleEquation(constants1)), ADDERS_1, MULTIPLIERS_1) == TARGET_VALUE_1)
+assert(getPasswordHash(next(solveSingleEquation(constants2)), ADDERS_2, MULTIPLIERS_2) == TARGET_VALUE_2)
 
-candidate = solve(constants)
-print(hex(getPasswordHash(candidate, ADDERS_1, MULTIPLIERS_1)))
+#print([hex(x) for x in constants1])
+#print([str(x) for x in constants2])
+#print(factorizeConstants(constants1))
 
-print([hex(x) for x in constants])
+# This is really wack, I should only need to call "solveSingleEquation" once to
+# get an initial setup value, but I'm lazy and my code in "solveBothEquations"
+# is bad, so whatever
+for p in solveSingleEquation(constants1):
+    password = p
+    solveBothEquations()
+
+
+#takenValues = [0] * 65536
+#takenCount = 0
+#for i in range(65536):
+#    passwd = [0] * 10
+#    passwd[4] = i
+#    h = getPasswordHash(passwd, ADDERS_1, MULTIPLIERS_1)
+#    if not takenValues[h]:
+#        takenValues[h] = 1
+#        takenCount += 1
+#print(takenCount)
